dc1c461e2142a2bd057c27deb146d8c79e147d24
Jairo Junior <junior.jairo1@gmail.com>
2017-05-25 20:16:04 -0300

    Listen to the warnings.

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,13 +4,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_wildfly_running_and_enabled(Service):
-    wildfly = Service('wildfly')
+def test_wildfly_running_and_enabled(host):
+    wildfly = host.service('wildfly')
 
     assert wildfly.is_enabled
 
 
-def test_wildfly_listening_http(Socket):
-    socket = Socket('tcp://0.0.0.0:8080')
+def test_wildfly_listening_http(host):
+    socket = host.socket('tcp://0.0.0.0:8080')
 
     assert socket.is_listening
